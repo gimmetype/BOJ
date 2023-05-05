d24a4f5e8c41c3fef54835f565913968f2449787
김미형 <gimmetype@gmail.com>
2023-05-05 17:45:47 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    double  euclidean = 0;
+    euclidean = N * N * M_PI;
+
+    double taxi = 0;
+    taxi = 2 * N * N;
+
+
+    printf("%f\n", euclidean);
+    printf("%f", taxi);
+
+
+    return 0;
+}
