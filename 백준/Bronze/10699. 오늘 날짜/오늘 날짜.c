b67ae681e2ad278fb973a22b6260249bb0d9cf57
김미형 <gimmetype@gmail.com>
2023-05-10 23:08:14 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void) {
+    struct tm* t;
+    time_t base = time(NULL);
+
+    t = localtime(&base);
+
+    printf("%d-%02d-%02d", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
+
+    return 0;
+
+}
+
+
