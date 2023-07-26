ab8443fc8823ee7c4464fa98440aceb3603564fb
김미형 <gimmetype@gmail.com>
2023-07-26 16:33:56 +0900

    [Silver V] Title: 나이순 정렬, Time: 64 ms, Memory: 13224 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int age;
+    char name[101];
+} member;
+
+int compare(member* a, member* b) {
+    return (a->age > b->age ? 1 : (a->age < b->age ? -1: 0));
+}
+
+int main(void){
+    int N;
+    scanf("%d", &N);
+    member* arr = (member*)malloc(N * sizeof(member));
+    for (int i = 0; i < N; i++) {
+        scanf("%d %s", &arr[i].age, arr[i].name);
+    }
+    qsort(arr, N, sizeof(member), compare);
+    for (int i = 0; i < N; i++) {
+        printf("%d %s\n", arr[i].age, arr[i].name);
+    }
+    return 0;
+}
