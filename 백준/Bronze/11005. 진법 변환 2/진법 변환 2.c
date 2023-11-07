fd33ccf258f8720278b0aa9f3d4488022651b2ba
김미형 <gimmetype@gmail.com>
2023-11-07 23:47:32 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void convert(int N, int B){
+    if (N != 0){
+        convert(N / B, B);
+        if (N % B > 9 && B > 10){
+            printf("%c", 'A' + N % B - 10);
+        }
+        else
+            printf("%d", N % B);
+    }
+}
+
+int main(){
+    int N, B;
+
+    scanf("%d %d", &N, &B);
+    convert(N, B);
+
+    return 0;
+}
